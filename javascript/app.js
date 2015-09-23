79fb3e4a7a0003613153a36fc98850f5e9617db5
ekam1 <markus-juopperi@hotmail.com>
2015-09-23 17:06:28 +0300

    First commit

---
+++ javascript/app.js
@@ -0,0 +1,48 @@
+/**
+ * @author markusjuopperi
+ */
+var app = angular.module('kauppapaikka', ['ngResource', 'angularUtils.directives.dirPagination']);
+var api_url = "http://mepa-store-api.herokuapp.com/marketads/:id";
+
+app.factory("Entry", function($resource) {
+	return $resource(api_url);
+});
+
+app.controller('ResourceController', function($scope, Entry) {
+
+	var entry = Entry.get({
+		id : $scope.id
+	}, function() {
+		console.log(entry);
+	});
+	// get() returns a single entry
+
+	var entries = Entry.query(function() {
+		console.log(entries);
+	});
+	//query() returns all the entries
+	$scope.ads = entries;
+	$scope.entry = new Entry();
+	//You can instantiate resource class
+
+	$scope.entry.data = 'some data';
+
+	Entry.save($scope.entry, function() {
+		//data saved. do something here.
+	});
+	//saves an entry. Assuming $scope.entry is the Entry object
+});
+
+app.controller('NavBarController', function($scope) {
+	this.tab = 1;
+	this.selectTab = function(setTab) {
+		this.tab = setTab;
+
+	};
+	this.isSelected = function(checkTab) {
+		return this.tab === checkTab;
+	};
+});
+
+
+
